d3d4086f5f74db0f33824ce6f08feb57f14d0c34
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-31 17:10:23 +0100

    Started working on the navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
+import imageProfile from "./assets/images/profile-pic.JPG";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <img src={imageProfile} />
     </div>
   );
 }
