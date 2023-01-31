83e80096d8f695afc25315e5e215e19afbfd7bca
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-31 13:55:07 +0100

    Started working on the navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import myProfile from "../../assets/images/profile-pic.JPG";
 import "./NavBar.css";
 
 const NavBar = () => {
   return (
     <div className="nav-bar-container">
-      <div className="nav-list"></div>
+      <div className="nav-details">
+        <div className="my-image">
+          <img src={myProfile} alt="Dev Olagboye Art" />
+        </div>
+        <div className="nav-list">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
