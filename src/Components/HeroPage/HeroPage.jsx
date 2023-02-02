35b33236374aa924938f9d07bc753e201f432773
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-02 20:45:57 +0100

    Started working on the Heropage component

--- src/Components/HeroPage/HeroPage.jsx
+++ src/Components/HeroPage/HeroPage.jsx
@@ -4,7 +4,7 @@ import "./HeroPage.css";
 const HeroPage = () => {
   return (
     <div className="heropage-container">
-      <div>
+      <div className="main-text">
         <h6>Akingbola Olagboye</h6>
         <p>I design & build interfaces</p>
       </div>
