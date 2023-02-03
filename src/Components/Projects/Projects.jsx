b0f2930834d3b3844ac90f76c8e5e1bbd5eabef8
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-03 17:51:22 +0100

    Started working on the Projects Component

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./Projects.css";
 
 const Projects = () => {
-  return <div></div>;
+  return <div className="projects-container">
+        <div>
+            <h6>Selected Projects</h6>
+        </div>
+  </div>;
 };
 
 export default Projects;
