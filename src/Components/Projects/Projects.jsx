e527e78f1ea213022be66ccfa57547fee1616c46
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-03 17:24:48 +0100

    Started working on the Projects Component

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import "./Projects.css";
 
 const Projects = () => {
-  return (
-    <div>Projects</div>
-  )
-}
+  return <div>Projects</div>;
+};
 
-export default Projects
+export default Projects;
