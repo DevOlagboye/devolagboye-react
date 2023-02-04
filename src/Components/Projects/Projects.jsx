273fd6021fc80035abf9d921e8e697ba7e1a97c7
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-04 16:40:54 +0100

    Made some changes to the HeroPage component

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -28,6 +28,26 @@ const Projects = () => {
         </div>
         
       </div>
+      <div className="project-details">
+        <div className="year">
+          <p>2023</p>
+        </div>
+        <div className="project-main-details">
+          <h5 className="project-name">Apollo NFT MarketPlace</h5>
+          <p className="project-details">
+            Apollo NFT MarketPlace is a project I designed after my interest in
+            web3 and its ecosystem.
+          </p>
+          <p className="project-details">
+            I got the inspiration for this from the figma community.
+          </p>
+          <a href="##" className="case-study-link">Read case study</a>
+          <a className="image-link" href="https://nft-marketplace-landing-page.netlify.app/">
+            <img src={nftLandPage} alt="NFT Land page"  className="project-image"/>
+          </a>
+        </div>
+        
+      </div>
     </div>
   );
 };
