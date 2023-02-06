5eabd5b4bd3a61c08375fd95215fe7aa639f9233
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-06 19:07:20 +0100

    Added the image to the about component and Started the About Component design

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./About.css"
 import firstAboutImage from "../../assets/images/devolagboye.jpg"
+import myOutingImage from "../../assets/images/myouting-devolagboye.JPG"
 
 const About = () => {
   return (
@@ -8,6 +9,9 @@ const About = () => {
         <div className="about-image-container">
             <div className='about-first-image'>
                 <img src={firstAboutImage} alt="" />
+            </div>
+            <div className='about-second-image'>
+                <img src={myOutingImage} alt="" />
             </div>
         </div>
     </div>
