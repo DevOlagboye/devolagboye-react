58353d71020dfd6eee37f8305bae39db4910ef49
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-07 14:15:52 +0100

    Started working on the arrangement of the images on the About Component

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./About.css";
 import firstAboutImage from "../../assets/images/devolagboye.jpg";
 import myOutingImage from "../../assets/images/myouting-devolagboye.JPG";
-import myDevFestImage from "../../assets/images/devolagboye devfest.JPG"
+import myDevFestImage from "../../assets/images/devolagboye devfest.JPG";
 
 const About = () => {
   return (
@@ -19,6 +19,14 @@ const About = () => {
         </div>
         <div className="about-fourth-image">
           <img src={myDevFestImage} alt="" />
+        </div>
+      </div>
+      <div className="about-me-container">
+        <div className="about-me-title">
+          <h5>About me</h5>
+        </div>
+        <div className="about-me-details">
+            <h5><span>Hi there!</span>I'm Olagboye, a frontend engineer with passion & love for designs and its aesthetics. I love building simple and smooth interfaces.</h5>
         </div>
       </div>
     </div>
