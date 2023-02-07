6c19a45e303a750b98456a5d9d860549d2a3155a
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-07 18:08:41 +0100

    Added the Github link

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -57,8 +57,8 @@ const About = () => {
         </div>
         <div className="connect-links">
           <a href="##">Email</a>
-          <a href="##">Twitter</a>
-          <a href="##">GitHub</a>
+          <a href="https://twitter.com/devolagboye">Twitter</a>
+          <a href="https://github.com/Akingbola1234">GitHub</a>
         </div>
       </div>
     </div>
