c5e29f948f9844b86214feb131cb967cb5d6095c
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-14 12:37:30 +0100

    Started working on the contact component

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,8 +1,15 @@
 import React from "react";
-import "./Contact.css"
+import "./Contact.css";
 
 const Contact = () => {
-  return <div className="contact-container">Contact</div>;
+  return (
+    <div className="contact-container">
+      <div className="contact-details">
+        <h5>Hi there, if you will be looking into connecting with me, we can connect on <a href="##">Github</a> You could follow me on <a href="##">Twitter</a> I post about Tech and Frontend we can collaborate on <a href="##">Github</a></h5>
+        <h5>I recently got interested in Open Source as well as Web3 if you will be looking into us collaborating on a project feel free to reach me on any of the platform above.</h5>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
