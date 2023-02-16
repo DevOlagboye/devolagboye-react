9ae7250c862833af599fbb4daf60682ce24cbf34
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-16 16:01:59 +0100

    Removed some codes from the index.css

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,43 +1,31 @@
-import React, {useState, useRef} from "react";
+import React, { useState, useRef } from "react";
 import myProfile from "../../assets/images/profile-pic.JPG";
 import { NavLink } from "react-router-dom";
-import { Dropdown } from "antd";
+import { BsFillMoonStarsFill, BsFillBrightnessHighFill } from "react-icons/bs";
 import "./NavBar.css";
 
-const items = [
-  {
-    label: <p>Automatic</p>,
-    key: "0",
-  },
-  {
-    label: <p>Light</p>,
-    key: "1",
-  },
-  {
-    label: <p>Dark</p>,
-    key: "2",
-  },
-];
 const NavBar = () => {
+  let [checkActive, setActive] = useState(true);
+  let svgRef = useRef(null);
+  const handleActive = () => {
+    svgRef.current.classList.add("active");
+    console.log(svgRef.current);
+    document.body.style.background = "#FFF";
+  };
+  const handleRemove = () => {
+    svgRef.current.classList.remove("active");
+    document.body.style.background = "#161615";
+  };
 
-  let [checkActive, setActive] = useState(false) 
-  let svgRef = useRef(null)
-  const handleActive = () =>{
-      svgRef.current.classList.add("active")
-  }
-  const handleRemove = () =>{
-      svgRef.current.classList.remove("active")
-  }
-
-  let handleClick = () =>{
-      if(checkActive === false){
-        handleActive();
-        checkActive = true
-      }else{
-        handleRemove()
-        checkActive = false
-      }
-  }
+  let handleClick = () => {
+    if (checkActive === false) {
+      handleActive();
+      checkActive = true;
+    } else {
+      handleRemove();
+      checkActive = false;
+    }
+  };
 
   return (
     <div className="nav-bar-container">
@@ -86,29 +74,18 @@ const NavBar = () => {
           </ul>
         </div>
         <div>
-          <Dropdown
-            menu={{ items, selectable: true, defaultSelectedKeys: ["0"] }}
-            trigger={["click"]}
+          <button
+            className="nav-list-button"
+            ref={svgRef}
+            onClick={handleClick}
+            class="svg"
           >
-            <button
-              className="nav-list-button"
-              onClick={(e) => e.preventDefault()}
-            >
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                class="svg" ref={svgRef} onClick={handleClick}
-              >
-                <path
-                  d="M12 3.5C7.30558 3.5 3.5 7.30558 3.5 12C3.5 16.6944 7.30558 20.5 12 20.5C16.4176 20.5 20.0476 17.1303 20.4608 12.8207C20.4801 12.6202 20.377 12.4277 20.1995 12.3324C20.0219 12.2372 19.8045 12.2578 19.6481 12.3848C18.7884 13.0824 17.6937 13.5 16.5 13.5C13.7386 13.5 11.5 11.2614 11.5 8.5C11.5 6.8599 12.2892 5.40423 13.5106 4.49167C13.6721 4.37101 13.7453 4.16516 13.6963 3.96961C13.6473 3.77406 13.4857 3.62706 13.2864 3.59678C12.8666 3.53302 12.437 3.5 12 3.5Z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-            </button>
-          </Dropdown>
+            {checkActive ? (
+              <BsFillMoonStarsFill className="svg" />
+            ) : (
+              <BsFillBrightnessHighFill className="svg" />
+            )}
+          </button>
         </div>
       </div>
     </div>
