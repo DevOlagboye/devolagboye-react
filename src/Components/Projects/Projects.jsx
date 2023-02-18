be8f3cd336567f39817d205c0f9429b87a893164
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-18 12:44:25 +0100

    Started working on the external links of the project

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -53,7 +53,7 @@ const Projects = () => {
           <p className="project-details">
             I got the inspiration for this from the figma community.
           </p>
-          <a href="##" className="case-study-link">
+          <a href="https://aviation-landingpage.netlify.app/" className="case-study-link">
             Visit Live <AiOutlineRise className="icon" />
           </a>
           <a
