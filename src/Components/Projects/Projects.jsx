a728cd8bd6f2bcbc90695d18e4bf1e97e46934d5
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-19 15:41:14 +0100

    Started working on animation

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -4,6 +4,7 @@ import { AiOutlineRise } from "react-icons/ai";
 import nftLandPage from "../../assets/images/Landing Page NFT - Apollo NFT (Community).jpg";
 import aviationLandPage from "../../assets/images/01_Aviation landing page 1.jpg";
 import cocoinLandPage from "../../assets/images/cocoin.jpg";
+import { motion } from "framer-motion";
 
 const Projects = () => {
   return (
