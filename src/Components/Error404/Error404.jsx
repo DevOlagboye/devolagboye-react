77d0139fcd30fe45c8829992b4dfd4c9f9fb14e0
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-01 15:59:38 +0100

    Started Styling the Error 404 Page

--- src/Components/Error404/Error404.jsx
+++ src/Components/Error404/Error404.jsx
@@ -4,7 +4,10 @@ import "./Error404.css";
 const Error404 = () => {
   return (
     <div className="error-container">
-      <div className="error-text"></div>
+      <div className="error-text">
+        <h3>404</h3>
+        <h4>Page not found!</h4>
+      </div>
     </div>
   );
 };
