18885a81be630d5c95dbef6337f3d5dfe2300a23
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-01 16:15:29 +0100

    Started Styling the Error 404 Page

--- src/Components/Error404/Error404.jsx
+++ src/Components/Error404/Error404.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Error404.css";
+import { FaArrowLeft } from "react-icons/fa";
 
 const Error404 = () => {
   return (
@@ -7,7 +8,9 @@ const Error404 = () => {
       <div className="error-text">
         <h3>404</h3>
         <h4>Page not found!</h4>
-        <a href="/" className="error-link">Go to homepage</a>
+        <a href="/" className="error-link">
+          <FaArrowLeft className="error-icon" /> Go to homepage
+        </a>
       </div>
     </div>
   );
