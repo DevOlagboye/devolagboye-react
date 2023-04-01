bc6448ec6c043f7ba7e0f20212eae2911cc598ff
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-01 15:58:47 +0100

    Added required font for the Error 404 Page

--- src/Components/Error404/Error404.jsx
+++ src/Components/Error404/Error404.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import "./Error404.css";
 
 const Error404 = () => {
-  return <div className="error-container">
-    
-  </div>;
+  return (
+    <div className="error-container">
+      <div className="error-text"></div>
+    </div>
+  );
 };
 
 export default Error404;
